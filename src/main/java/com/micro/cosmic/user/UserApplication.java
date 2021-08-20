da99607e16fbda956a93b8729b99a47f504f7b4c
caojiancheng <jiancheng.cao@yi23.net>
2021-08-20 23:42:29 +0800

    添加ignore

---
+++ src/main/java/com/micro/cosmic/user/UserApplication.java
@@ -0,0 +1,18 @@
+package com.micro.cosmic.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author caojiancheng
+ * @date 2021/8/20
+ * @description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}

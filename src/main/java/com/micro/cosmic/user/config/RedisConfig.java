8a1f5c37e53560c7cebfb5ab57e6d0a3187ac681
caojiancheng <jiancheng.cao@yi23.net>
2021-08-21 22:27:38 +0800

    添加mysql和mabatis整合分布式配置

---
+++ src/main/java/com/micro/cosmic/user/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.micro.cosmic.user.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author caojiancheng
+ * @date 2021/8/21
+ * @description
+ */
+@Configuration
+public class RedisConfig {
+    @Autowired
+    private RedisConnectionFactory factory;
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
+    }
+}
--- src/main/java/com/micro/cosmic/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/micro/cosmic/user/service/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.micro.cosmic.user.service.impl;
 
 import com.micro.cosmic.user.dao.UserDAO;
 import com.micro.cosmic.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -17,8 +19,12 @@ public class UserServiceImpl implements UserService {
     @Resource
     private UserDAO userDAO;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Override
     public String getUser(Integer userId) {
+        System.out.println(redisTemplate.opsForValue().get("name"));
         return userDAO.selectUserById(userId).toString();
     }
 }
